2e97cf7532a9016eb8dd75988e02cad560103e41
sasha <sasha.kruglyak@lemonade.com>
2021-04-30 10:06:08 +0300

    add ranger

--- install/macos.sh
+++ install/macos.sh
@@ -3,3 +3,14 @@
 ### ranger install
 brew install ranger
 ranger --copy-config=all
+
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
+
+git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
+# don't forger to set image preview to false
+# otherwise it will stuck on a big files
+
+# In iterm: ITerm2 > Preferences > Profiles > Text > Change Font
+# and select nerd font
