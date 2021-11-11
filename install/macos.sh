9082c7ccd50ea334a232e2e5c5547c579b33e62e
Sasha Kruglyak <zooblin@gmail.com>
2021-11-11 09:13:48 +0200

    more installs

--- install/macos.sh
+++ install/macos.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git config --global user.email "zooblin@gmail.com"
+git config --global user.name "Sasha Kruglyak"
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
