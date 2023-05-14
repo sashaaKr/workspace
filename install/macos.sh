b8de2bc46a50aed8e90b65a2881fe4e229706a0f
sasha <zooblin@gmail.com>
2023-05-14 09:41:47 +0300

    git pretty diff

--- install/macos.sh
+++ install/macos.sh
@@ -45,14 +45,10 @@ $(brew --prefix)/opt/fzf/install
 
 brew install gh	
 
+
+#### starship
 brew install starship
 echo "eval "$(starship init zsh)"" >> ~/.zshrc
-
-brew install awscli
-
-brew install kubectl 
-echo "source <(kubectl completion zsh)" >> ~/.zshrc
-
 cat <<EOT >> ~/.config/starship.toml
 [kubernetes]
 symbol = "⎈ "
@@ -60,6 +56,13 @@ format = " [\$symbol\$context( \\\(\$namespace\\\))](\$style) "
 style = "bright-blue"
 disabled = false
 EOT
+
+
+brew install awscli
+
+brew install kubectl 
+echo "source <(kubectl completion zsh)" >> ~/.zshrc
+
 
 brew install kubectx
 brew install k9s
@@ -74,3 +77,23 @@ pip3 install yq
 gh extension install dlvhdr/gh-dash
 
 
+### git pretty diff
+brew install diff-so-fancy
+
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global interactive.diffFilter "diff-so-fancy --patch"
+git config --global color.ui true
+
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta       "11"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.func       "146 bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
+### END
