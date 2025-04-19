277755eae6b91dfd97fdd94b93c6802444041572
Test User <test@example.com>
2025-04-19 16:47:01 +0300

    git alias

--- install/macos.sh
+++ install/macos.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-git config --global user.email "zooblin@gmail.com"
-git config --global user.name "Sasha Kruglyak"
+source "$(dirname "$0")/git_setup.sh"
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -100,33 +99,5 @@ brew install redis
 
 # https://github.com/dlvhdr/gh-dash
 gh extension install dlvhdr/gh-dash
-
-
-### git pretty diff
-brew install diff-so-fancy
-
-git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
-git config --global interactive.diffFilter "diff-so-fancy --patch"
-git config --global color.ui true
-
-git config --global color.diff-highlight.oldNormal    "red bold"
-git config --global color.diff-highlight.oldHighlight "red bold 52"
-git config --global color.diff-highlight.newNormal    "green bold"
-git config --global color.diff-highlight.newHighlight "green bold 22"
-
-git config --global color.diff.meta       "11"
-git config --global color.diff.frag       "magenta bold"
-git config --global color.diff.func       "146 bold"
-git config --global color.diff.commit     "yellow bold"
-git config --global color.diff.old        "red bold"
-git config --global color.diff.new        "green bold"
-git config --global color.diff.whitespace "red reverse"
-
-#https://blog.gitbutler.com/how-git-core-devs-configure-git
-git config --global column.ui auto
-git config --global branch.sort -committerdate
-git config --global diff.algorithm histogram
-
-echo 'alias commit_wip="git add . && git commit --amend --no-edit && ggpush --force-with-lease"' >> ~/.zshrc
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
